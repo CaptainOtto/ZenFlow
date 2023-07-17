ed7af4b44214fca393bef11fd8a0f27b0185f1d2
CapnJ <jottossonta@gmail.com>
2023-07-17 08:48:50 +0200

    Text button adjustments - fixed text button color states. - Fixed text button onclick callback. - Added button logging.

--- src/lib/buttons/TextButton.svelte
+++ src/lib/buttons/TextButton.svelte
@@ -1,17 +1,23 @@
-<script lang="ts">
+<script lang='ts'>
+
     export let text: string = 'Button';
     export let onClick: (() => void) | undefined;
+    export let name: string | undefined;
 
     function handleClick() {
-     if (typeof onClick === 'function') {
-      onClick();
+      if (typeof onClick === 'function') {
+        onClick();
+      }
+
+      console.debug(`Button ${name} clicked`);
     }
 
-    console.log('Button clicked');
-  }
   </script>
   
-  <button class="bg-gradient-to-t from-orange-700 to-orange-200 text-white font-bold py-2 px-10 rounded-full hover:from-orange-500 hover:to-orange-200"
+  <button class='bg-gradient-to-t from-orange-700 to-orange-200 text-white font-bold py-2 px-10 rounded-full 
+      hover:from-orange-500 hover:to-orange-200 
+      active:from-orange-700 active:to-orange-400
+    '
     on:click={handleClick}>
     {text}
   </button>
--- src/views/welcome-page/WelcomeView.svelte
+++ src/views/welcome-page/WelcomeView.svelte
@@ -1,20 +1,25 @@
 <script>
-    import TextAndIconLogo from "../../lib/logo/TextAndIconLogo.svelte";
-    import TextButton from "./../../lib/buttons/TextButton.svelte";
+    import TextAndIconLogo from '../../lib/logo/TextAndIconLogo.svelte';
+    import TextButton from './../../lib/buttons/TextButton.svelte';
+
+    // Handles getting started button click
+    function handleClick() {
+    }
+
 </script>
 
-<div class="h-full">
-    <div class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3">
+<div class='h-full'>
+    <div class='absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3'>
         <TextAndIconLogo/>
     </div>
 
-    <div class="flex justify-center items-center h-full">
+    <div class='flex justify-center items-center h-full'>
 
-        <div class="mt-auto flex flex-col justify-center items-center" style="margin-bottom: 100px">
-            <TextButton text="Getting Started" />
-            <div class="flex flex-row justify-center items-center mt-10">
-                <p class="text-base text-white font-normal mr-5">Click to learn about</p>
-                <p class="text-base text-white font-bold ">Zen Flow</p>
+        <div class='mt-auto flex flex-col justify-center items-center' style='margin-bottom: 100px'>
+            <TextButton name='getting-started' onClick={handleClick} text='Getting Started' />
+            <div class='flex flex-row justify-center items-center mt-10'>
+                <p class='cursor-pointer text-base text-white font-normal mr-5'>Click to learn about</p> 
+                <p class='cursor-pointer text-base text-white font-bold '>Zen Flow</p> 
             </div>
         </div>
     </div>
